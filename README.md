a4401584f7840eedd0e1f08cbfd16091d094c127
Marcus Hughes <hello@msh100.uk>
2021-11-14 17:52:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Superhub 4 Restarter
+
+This utility is designed to reboot the Superhub 4
+[when it enters a bad state](https://github.com/msh100/modem-stats/issues/2),
+using a TP-Link Smart Plug.
+
+
+## Supported Smart Plugs
+
+This utility depends on
+[python-kasa](https://github.com/python-kasa/python-kasa) and therefore
+supports all of the smart plugs that are supported there.
+
+ * HS100
+ * HS103
+ * HS105
+ * HS107
+ * HS110
+ * KP105
+ * KP115
+ * KP401
+
+
+## Usage
+
+Firstly, it's important to remember that this utility will hard reboot the
+Superhub 4 at the wall.
+Because of this, this utility is only designed to work in Modem Mode, where the
+network is still functional (without internet) during the whole process.
+
+We address the Smart Plug directly.
+Therefore it's important that its IP address is statically defined on your DHCP
+server.
+
+
+### Configuration
+
+Variable     | Description
+-------------|-------------
+`PLUG_IP`    | The IP address of the Kasa Smart Plug
+`START_HOUR` | The start of the timeframe this script is allowed to reboot.
+`END_HOUR`   | The end of the timeframe this script is allowed to reboot.
+
+
+### Docker
+
+```yaml
+version: "2.1"
+services:
+  sh4-restarter:
+    image: msh100/sh4-restarter
+    restart: unless-stopped
+    environment:
+        PLUG_IP: 192.168.1.102
+        START_HOUR: 2
+        END_HOUR: 7
+```
---
+++ sh4-restarter.py
@@ -0,0 +1,101 @@
+import requests
+import json
+from datetime import datetime, timedelta
+import time
+import asyncio
+import logging
+import os
+
+from kasa import SmartPlug
+
+def getSH4Stats(superhub):
+    try:
+        r = requests.get(f"http://{superhub}/php/ajaxGet_device_networkstatus_data.php")
+        return json.loads(r.content)
+    except:
+        return False
+
+def sh4Functional(superhub):
+    stats = getSH4Stats(superhub)
+    if not stats:
+        return False
+
+    if len(stats[3]) > 0:
+        return True
+    return False
+
+def sh4Test(superhub):
+    for _ in range(5):
+        if sh4Functional(superhub):
+            return True
+        time.sleep(10)
+    return False
+
+def sleepUntilActiveHour(startHour, endHour):
+    currentHour = datetime.now().hour
+    if currentHour >= startHour and currentHour < endHour:
+        return 0
+
+    tomorrow = datetime.now() + timedelta(1)
+    tomorrowStart = datetime(year=tomorrow.year, month=tomorrow.month,
+        day=tomorrow.day, hour=startHour, minute=0, second=5)
+    return (tomorrowStart - datetime.now()).seconds
+
+async def asyncRestartSH4(smartPlug):
+    plug = SmartPlug(smartPlug)
+
+    await plug.update()
+    while plug.is_on:
+        await plug.turn_off()
+        await plug.update()
+
+    time.sleep(1)
+
+    while not plug.is_on:
+        await plug.turn_on()
+        await plug.update()
+
+def restartSH4(smartPlug):
+    asyncio.run(asyncRestartSH4(smartPlug))
+
+def runMainLoop(superhub, smartPlug, startHour, endHour):
+    logging.debug(
+        'Entered main loop '
+        f'(Superhub: {superhub}, '
+        f'Kasa Plug: {smartPlug}, '
+        f'Run time: {startHour}-{endHour}h)',
+    )
+    while True:
+        nextLoop = sleepUntilActiveHour(startHour, endHour)
+        if nextLoop == 0:
+            if not sh4Test(superhub):
+                logging.info(f'Superhub 4 fails test, toggling power')
+                restartSH4(smartPlug)
+                logging.debug(f'Sleeping for 1 hour post Superhub 4 restart')
+                time.sleep(3600)
+            else:
+                logging.debug('Superhub 4 is functioning correctly')
+                time.sleep(60)
+        else:
+            logging.info(f'Sleeping until active hour ({nextLoop} seconds)')
+            time.sleep(nextLoop)
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info('Started')
+
+    if 'PLUG_IP' not in os.environ:
+        raise Exception('PLUG_IP is not defined')
+    smartPlug = os.environ.get('PLUG_IP')
+    startHour = os.environ.get('START_HOUR', 2)
+    endHour = os.environ.get('END_HOUR', 7)
+    smartPlug = os.environ.get('PLUG_IP')
+
+    runMainLoop(
+        superhub="192.168.100.1",
+        smartPlug=smartPlug,
+        startHour=startHour,
+        endHour=endHour,
+    )
+
+main()
